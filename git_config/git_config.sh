964f3c63b4689a42e0251df4ad5d31b1a1671368
Vasiliy Zukanov <vasiliy.zukanov@gmail.com>
2017-05-22 11:52:39 +0300

    Enhancing git configuration with several additional checks

--- git_config/git_config.sh
+++ git_config/git_config.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
+# Ensure git installed
+command -v git > /dev/null 2>&1 || { echo "Git not found. Aborting!"; return 1; }
+
+SYSTEM_GITCONFIG=~/.gitconfig
+MY_GITCONFIG=$GIT_CONFIG_DIR/gitconfig
+
+# Backup exisitng gitconfig if exists
+if [ -f "${SYSTEM_GITCONFIG}" ] ; then
+    DATE=`date -Iseconds`
+    GITCONFIG_BAK=~/gitconfig.bak.$DATE
+    echo Moving existing $SYSTEM_GITCONFIG to $GITCONFIG_BAK
+    mv $SYSTEM_GITCONFIG $GITCONFIG_BAK
+fi
+
 # Create symlink to gitconfig file
-ln -s $GIT_CONFIG_DIR/gitconfig ~/.gitconfig
+echo Creating symlink from $SYSTEM_GITCONFIG to $MY_GITCONFIG
+ln -s $MY_GITCONFIG $SYSTEM_GITCONFIG
--- source_me.sh
+++ source_me.sh
@@ -14,9 +14,13 @@ echo ----------------------------------------
 
 GIT_CONFIG_DIR=$ENV_CONFIG_DIR/git_config
 GIT_CONFIG_SCRIPT=$GIT_CONFIG_DIR/git_config.sh
+
 if [ -f "${GIT_CONFIG_SCRIPT}" ] ; then
     echo Git configuration script: $GIT_CONFIG_SCRIPT
-    source "${GIT_CONFIG_SCRIPT}"
+    source "${GIT_CONFIG_SCRIPT}" || return 1;
 else
     echo Git configuration script wasn´t found: $GIT_CONFIG_SCRIPT
+    return 1
 fi
+
+echo ----------------------------------------

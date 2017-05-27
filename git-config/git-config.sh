e14d586ac5afb4ecf582938b30cb10d28b7158c4
Vasiliy Zukanov <vasiliy.zukanov@gmail.com>
2017-05-27 18:42:58 +0300

    Enhancing git configuration in order to enable p4merge diff on cygwin

--- git-config/git-config.sh
+++ git-config/git-config.sh
@@ -6,6 +6,13 @@ source $ENV_CONFIG_DIR/bash-functions/createSymlinkBackupIfExists.sh
 # Ensure git installed
 command -v git > /dev/null 2>&1 || { echo "Git not found. Aborting!"; return 1; }
 
+# If running under Cygwin - need to set BASE_DIR env variable for p4merge to work
+
+if [[ `uname -o` == "Cygwin" ]] ; then
+    export BASE_DIR=`cygpath -w /`
+fi
+
+
 SYSTEM_GITCONFIG=~/.gitconfig
 MY_GITCONFIG=$ENV_CONFIG_DIR/git-config/gitconfig
 

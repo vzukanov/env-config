c49fffb11708c3be158c92a2e56875905a810975
Vasiliy Zukanov <vasiliy.zukanov@gmail.com>
2017-06-06 21:55:32 +0300

    Fixed p4merge paths issue in cygwin

--- git-config/git-config.sh
+++ git-config/git-config.sh
@@ -9,7 +9,7 @@ command -v git > /dev/null 2>&1 || { echo "Git not found. Aborting!"; return 1; 
 # If running under Cygwin - need to set BASE_DIR env variable for p4merge to work
 
 if [[ `uname -o` == "Cygwin" ]] ; then
-    export BASE_DIR=`cygpath -w /`
+    export PATH=$PATH:$ENV_CONFIG_DIR/git-config/cygwin/
 fi
 
 
